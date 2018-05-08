e0af2dacb3b348cd1f0f00e4085e3bd374f03cdb
ethan <ff@f.d>
2018-05-08 23:33:25 +0530

    removed sudo

--- chrome.sh
+++ chrome.sh
@@ -2,7 +2,7 @@ if [ ! $(id -u) ] ; then
     echo 'This script must be run with root privileges' >&2
         exit 1
 	fi
-	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 	sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 	apt update
 	apt -y install google-chrome-stable
