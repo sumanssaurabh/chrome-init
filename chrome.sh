859f75b4dbc3fa99ee83ce61787947b67388b511
Suman Saurabh <30201452+sumanssaurabh@users.noreply.github.com>
2018-05-09 22:30:50 +0530

    fixed

--- chrome.sh
+++ chrome.sh
@@ -1,7 +1,8 @@
-if [ ! $(id -u) ] ; then
-    echo 'This script must be run with root privileges' >&2
-        exit 1
-	fi
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
 	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 	sh -c 'echo "deb  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/chrome.list'
 	apt update
